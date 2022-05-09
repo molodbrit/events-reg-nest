be5e4ebd3e5f3d88d3fda6f37304bd1f24cc40d4
Andrey Margushin <Andrey.Margushin@south.rt.ru>
2022-05-09 12:00:08 +0300

    upd validate rules

--- src/events/dto/add-event.dto.ts
+++ src/events/dto/add-event.dto.ts
@@ -1,15 +1,19 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsInt, IsNotEmpty, Length } from 'class-validator';
 
 export class AddEventDto {
   @IsNotEmpty()
+  @IsInt()
   readonly eventTypeId: number;
 
   @IsNotEmpty()
+  @Length(1, 256)
   readonly locality: string;
 
   @IsNotEmpty()
+  @Length(1, 256)
   readonly eventDate: string;
 
   @IsNotEmpty()
+  @IsInt()
   readonly userId: number;
 }
--- src/events/dto/update-event.dto.ts
+++ src/events/dto/update-event.dto.ts
@@ -1,7 +1,23 @@
+import { IsNotEmpty, IsInt, IsBoolean, Length } from 'class-validator';
+
 export class UpdateEventDto {
+  @IsNotEmpty()
+  @IsInt()
   readonly eventTypeId: number;
+
+  @IsNotEmpty()
+  @Length(1, 256)
   readonly locality: string;
+
+  @IsNotEmpty()
+  @Length(1, 256)
   readonly eventDate: string;
+
+  @IsNotEmpty()
+  @IsBoolean()
   readonly active: boolean;
+
+  @IsNotEmpty()
+  @IsInt()
   readonly userId: number;
 }
